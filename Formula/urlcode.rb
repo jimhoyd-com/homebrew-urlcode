7cef8e9cd9288a85cf405520baaf7a470e5a3361
Jimmy Ho <jimhoyd@gmail.com>
2026-09-21 08:15:00 -0500

    chore: update urlcode formula to 0.4.6 (#1)

--- Formula/urlcode.rb
+++ Formula/urlcode.rb
@@ -5,8 +5,8 @@
 class Urlcode < Formula
   desc "Portable runtime for programmable URL behavior"
   homepage "https://github.com/jimhoyd-com/urlcode"
-  url "https://registry.npmjs.org/@jimhoyd/urlcode/-/urlcode-0.3.0.tgz"
-  sha256 "d099cdc2d36b817370e2cb25be32b0778ccb7224290324b6fb4e5727e69bc43c"
+  url "https://registry.npmjs.org/@jimhoyd/urlcode/-/urlcode-0.4.6.tgz"
+  sha256 "f14226081d0092bd986a35ecc883e07e4447be0d1795605857efa86dd9653cba"
   license "Apache-2.0"
 
   depends_on "node"
@@ -16,19 +16,11 @@ class Urlcode < Formula
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
-  def caveats
-    <<~EOS
-      Live short-link storage needs a Node build carrying the patched SQLite WAL
-      fix. Check it with:
-        urlcode doctor
-      Everything except live links runs on any supported Node build.
-    EOS
-  end
-
   test do
     report = JSON.parse(shell_output("#{bin}/urlcode doctor"))
     assert_equal "Apache-2.0", report["license"]
-    assert_equal "quickjs-wasm", report["functionSandbox"]
+    assert_equal "trusted-in-process", report["functionDefault"]
+    assert_equal "quickjs-wasm", report["sandboxEngine"]
 
     (testpath/"urlcode.yaml").write <<~YAML
       version: "1"
